19e1cd6ec1e6651209db7a1b9d25f40b2a3bddc9
Arthur Asatryan <biacoder@gmail.com>
2018-04-04 14:57:41 +0400

    Some boy scouting.

--- escommons-example/src/main/kotlin/org/biacode/escommons/example/MainApplication.kt
+++ escommons-example/src/main/kotlin/org/biacode/escommons/example/MainApplication.kt
@@ -1,17 +1,10 @@
 package org.biacode.escommons.example
 
 import org.biacode.escommons.core.starter.annotation.EnableEsCommons
-import org.biacode.escommons.core.test.configuration.EsCommonsTestAnnotationDrivenConfiguration
-import org.biacode.escommons.toolkit.component.EsCommonsClientWrapper
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-import java.util.*
 
 /**
  * Created by Arthur Asatryan.
@@ -29,25 +22,4 @@ class MainApplication {
             SpringApplication.run(MainApplication::class.java)
         }
     }
-}
-
-@Configuration
-@Import(EsCommonsTestAnnotationDrivenConfiguration::class)
-class MainApplicationCommandLineRunner : CommandLineRunner {
-
-    //region Dependencies
-    @Autowired
-    private lateinit var esCommonsClientWrapper: EsCommonsClientWrapper
-    //endregion
-
-    //region Public methods
-    override fun run(vararg p0: String?) {
-        esCommonsClientWrapper.deleteIndices("*")
-        val indexName = UUID.randomUUID().toString()
-        esCommonsClientWrapper.createIndex(indexName, "person")
-        esCommonsClientWrapper.addAlias(indexName, "person")
-        esCommonsClientWrapper.getIndices().forEach { println(it) }
-    }
-    //endregion
-
 }
